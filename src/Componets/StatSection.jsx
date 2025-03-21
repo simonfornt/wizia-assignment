06e1fe998f362311b535f98e68c8f58e3c4603cf
simonfotnt <simonpiar98@gamil.com>
2025-03-21 16:24:26 +0600

    fixed some section

--- src/Componets/StatSection.jsx
+++ src/Componets/StatSection.jsx
@@ -5,12 +5,12 @@ function StatSection() {
   return (
     <div className="relative font-monster px-4 md:px-20 py-8 md:py-16 bg-[#002228] overflow-hidden">
       {/* Background Image */}
-      <div className="absolute inset-0 w-full h-full">
-        <img src={frame3} alt="Background" className="w-full h-full object-cover opacity-20" />
+      <div className="absolute inset-0  h-full">
+        <img src={frame3} alt="Background" className="mx-auto h-full object-cover opacity-100" />
       </div>
 
       {/* Content */}
-      <div className="relative z-10 max-w-5xl mx-auto text-center md:text-left py-10 px-6 md:px-10">
+      <div className="relative z-10 max-w-3xl  text-left md:text-left py-10 px-6 md:px-10">
         <h1 className="text-white font-[600] text-2xl md:text-4xl  mb-4">
           Allocate effort where your reps make an impact.
         </h1>
@@ -24,29 +24,29 @@ function StatSection() {
 
         {/* Stats */}
         <div className="grid grid-cols-1 font-monster sm:grid-cols-2 md:grid-cols-3 gap-8">
-          <div className="text-left md:text-center">
+          <div className="text-left ">
             <span className="text-[#0FF1F6] font-[700] text-3xl md:text-5xl  block">
               32%
             </span>
-            <p className="text-white text-base font-[500] font-monster md:text-xl">
+            <p className="text-[#E9EEF1] text-base font-[500] font-monster md:text-xl">
               Improvement in Open Rate
             </p>
           </div>
 
-          <div className="text-left md:text-center">
+          <div className="text-left ">
             <span className="text-[#0FF1F6] text-3xl md:text-5xl font-bold block">
               75%
             </span>
-            <p className="text-white text-base font-[500] md:text-xl">
+            <p className="text-[#E9EEF1] text-base font-[500] md:text-xl">
               Improvement in Ramp Time
             </p>
           </div>
 
-          <div className="text-left md:text-center">
+          <div className="text-left ">
             <span className="text-[#0FF1F6] text-3xl md:text-5xl font-bold block">
               35%
             </span>
-            <p className="text-white font-[500] text-base md:text-xl">
+            <p className="text-[#E9EEF1] font-[500] text-base md:text-xl">
               Improvement in Meetings Booked
             </p>
           </div>
